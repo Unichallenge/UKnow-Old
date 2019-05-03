b8cd749857b0ebe841256ca3386d92929f54db35
katmpatz <t8150091@dias.aueb.gr>
2019-05-03 14:32:57 +0300

    components

--- unichallenge_client/src/app/app.module.ts
+++ unichallenge_client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule
